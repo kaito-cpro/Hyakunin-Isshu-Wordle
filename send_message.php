48f3a431eef764b997d507d0b02a9093c534bd41
kaito-cpro <blue7-go0112@docomo.ne.jp>
2022-02-01 18:16:19 +0900

    update

--- send_message.php
+++ send_message.php
@@ -19,10 +19,10 @@
                        user-scalable=yes" />
         <meta name="keywords" content="百人一首,Wordle,競技かるた" />
         <link rel="stylesheet" href="style.css" />
-        <script src="py/send_message.py"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/brython/3.8.8/brython.js" integrity="sha256-rA89wPrTJJQFWJaZveKW8jpdmC3t5F9rRkPyBjz8G04=" crossorigin="anonymous"></script>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/sweetalert2@8"></script>
+        <script src="py/send_message.py"></script>
         <script type="text/javascript" src="js/gather_data.js"></script>
         <script type="text/javascript" src="js/loading.js"></script>
     </head>
