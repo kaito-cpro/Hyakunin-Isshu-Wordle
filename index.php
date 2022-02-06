61a1a9b433bcc76efcff5d983c9f4d85abee6d21
kaito-cpro <blue7-go0112@docomo.ne.jp>
2022-02-06 16:19:42 +0900

    update

--- index.php
+++ index.php
@@ -25,11 +25,11 @@
         <meta name="twitter:description" content="百人一首の初句の5文字を当てるWordleです" />
         <meta name="twitter:image" content="https://drive.google.com/file/d/1N63jvw2i9f-bkBm0dO42XT5GW2bLV_DY/view?usp=sharing" />
         <meta name="keywords" content="百人一首,Wordle,競技かるた" />
-        <link rel="stylesheet" href="style.css" />
+        <link rel="stylesheet" href="style.css?202202061618" />
         <script src="https://cdnjs.cloudflare.com/ajax/libs/brython/3.8.8/brython.js" integrity="sha256-rA89wPrTJJQFWJaZveKW8jpdmC3t5F9rRkPyBjz8G04=" crossorigin="anonymous"></script>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/sweetalert2@8"></script>
-        <script type="text/javascript" src="js/main.js"></script>
+        <script type="text/javascript" src="js/main.js?202202061618"></script>
         <script type="text/javascript" src="js/loading.js"></script>
     </head>
     <body onload="brython(); createGameBoard(); createKeyboard(); showHowToUse(); showPopup();">
