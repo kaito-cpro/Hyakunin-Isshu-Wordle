097a396de347823900231693494084406ae0c42f
kaito-cpro <blue7-go0112@docomo.ne.jp>
2022-02-08 18:12:41 +0900

    update

--- how_to_use.php
+++ how_to_use.php
@@ -19,7 +19,7 @@
                        maximum-scale=2.0,
                        user-scalable=yes" />
         <meta name="keywords" content="百人一首,Wordle,競技かるた" />
-        <link rel="stylesheet" href="style.css?202202061624" />
+        <link rel="stylesheet" href="style.css?202202081808" />
         <script src="https://cdnjs.cloudflare.com/ajax/libs/brython/3.8.8/brython.js" integrity="sha256-rA89wPrTJJQFWJaZveKW8jpdmC3t5F9rRkPyBjz8G04=" crossorigin="anonymous"></script>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/sweetalert2@8"></script>
@@ -38,11 +38,11 @@
             </div>
         </form>
         
-        <div class="dot"></div><p style="padding:4px 0px; margin:1px 6px 1px 6px; line-height:21px;"><a href="https://www.powerlanguage.co.uk/wordle/">Wordle</a>の百人一首版です。5回まで回答することができるので、100首の中で、初句(歌の一番はじめの句)がちょうど5文字であるような歌を当ててください。たとえば「ちはやぶる」は答えの札になり得ますが、「たきのおとは」は初句が6文字なので答えにはなり得ません。また「わたのはら」などは競技かるたでの決まり字は6字ですが、初句は5文字なので問題の答えになり得ることに注意してください。</p>
-        <div class="dot"></div><p style="padding:4px 0px; margin:1px 6px 1px 6px; line-height:21px;">回答するときは「あいうえお」のような、百人一首の中に存在しない歌は回答することができません。また仮名遣いは歴史的仮名遣いで入力するようにお願いします。</p>
-        <div class="dot"></div><p style="padding:4px 0px; margin:1px 6px 1px 6px; line-height:21px;">制限時間は5分です。百人一首Wordleは答えがすぐに絞れてしまうことが多いので、難易度調整のために制限時間を設けています。</p>
+        <div class="dot"></div><p style="padding:4px 0px; margin:1px 6px 1px 6px; line-height:21px;"><a href="https://www.powerlanguage.co.uk/wordle/">Wordle</a>の百人一首版です。5回まで回答することができるので、100首の中で、<span class="under">初句(歌の一番はじめの句)がちょうど5文字であるような歌</span>を当ててください。たとえば「ちはやぶる」は答えの札になり得ますが、「たきのおとは」は初句が6文字なので答えにはなり得ません。また「わたのはら」などは競技かるたでの決まり字は6字ですが、初句は5文字なので問題の答えになり得ることに注意してください。</p>
+        <div class="dot"></div><p style="padding:4px 0px; margin:1px 6px 1px 6px; line-height:21px;">回答するときは、百人一首の中に存在しない「あかさたな」などの5文字は回答することができません。また仮名遣いは<span class="under">歴史的仮名遣いで入力</span>するようにお願いします。</p>
+        <div class="dot"></div><p style="padding:4px 0px; margin:1px 6px 1px 6px; line-height:21px;">制限時間は5分です。百人一首Wordleはその性質上、答えがすぐに絞れてしまうことが多いので、難易度調整のために制限時間を設けています。</p>
         <div class="dot"></div><p style="padding:4px 0px; margin:1px 6px 1px 6px; line-height:21px;">回答した歌と答えの歌を比較して、場所も音も正しいときには緑色、場所は違うけどどこかにその音があるときには黄色、どこにもその音がないときには灰色で表示されます。</p>
-        <div class="dot"></div><p style="padding:4px 0px; margin:1px 6px 1px 6px; line-height:21px;">問題の答えは0時になると1日ごとに切り替わります。5回以内に正解できなくても1日に何度でも挑戦可能なので、いっぱい楽しんでください!</p>
+        <div class="dot"></div><p style="padding:4px 0px; margin:1px 6px 1px 6px; line-height:21px;"><span class="under">問題の答えは0時になると1日ごとに切り替わります</span>。5回以内に正解できなくても1日に何度でも挑戦可能なので、いっぱい楽しんでください!</p>
             
         <br>
         <br>
